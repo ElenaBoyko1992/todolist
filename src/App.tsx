93897968d2a9d1a0be8dd797de41e27edde37561
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-15 22:35:49 +0300

    lesson 1 (15.08.2022)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
 
 function App() {
+    //BLL (бизнес-логика):
+    const todoListTitle_1: string = "What to learn today"
+    const todoListTitle_2: string = "What to learn next week"
+    const tasks_1: Array<TaskType> = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS&TS", isDone: true},
+        {id: 3, title: "REACT", isDone: false},
+    ]
+    const tasks_2: Array<TaskType> = [
+        {id: 4, title: "Sass", isDone: false},
+        {id: 5, title: "MobX", isDone: false},
+        {id: 6, title: "Vue.js", isDone: false},
+    ]
+
+
+
+    //UI:
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList
+                title={todoListTitle_1}
+                tasks={tasks_1}
+            />
+
+            <TodoList
+                title={todoListTitle_2}
+                tasks={tasks_2}
+            />
+
         </div>
     );
 }
