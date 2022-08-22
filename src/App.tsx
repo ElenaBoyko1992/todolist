0144fea45f1b7b8cb8b225c836fea5653ace5a65
ElenaBoyko1992 <Bionka@inbox.ru>
2022-08-22 22:09:05 +0300

    lesson 2 (22.08.2022)

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
 
+//CLI - интерфейс командной строки
+//GUI - графический интерфейс - create, read, update, delete
+
+export type FilterValuesType = "all" | "active" | "completed"
+
 function App() {
     //BLL (бизнес-логика):
-    const todoListTitle_1: string = "What to learn today"
-    const todoListTitle_2: string = "What to learn next week"
-    const tasks_1: Array<TaskType> = [
+    const todoListTitle: string = "What to learn today"
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS&TS", isDone: true},
         {id: 3, title: "REACT", isDone: false},
-    ]
-    const tasks_2: Array<TaskType> = [
-        {id: 4, title: "Sass", isDone: false},
-        {id: 5, title: "MobX", isDone: false},
-        {id: 6, title: "Vue.js", isDone: false},
-    ]
+    ])
+    const [filter, setFilter] = useState<FilterValuesType>("all")
 
-
+    const removeTask = (taskID: number) => {
+        setTasks(tasks.filter(t => t.id !== taskID))
+        //  console.log(tasks) //работает асинхронно!!!
+    }
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    }
+    const getTasksForTodoList = () => {
+        switch (filter) {
+            case "active":
+                return tasks.filter(t => !t.isDone)
+            case "completed":
+                return tasks.filter(t => t.isDone)
+            default:
+                return tasks
+        }
+    }
 
     //UI:
     return (
         <div className="App">
             <TodoList
-                title={todoListTitle_1}
-                tasks={tasks_1}
+                title={todoListTitle}
+                tasks={getTasksForTodoList()}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
             />
-
-            <TodoList
-                title={todoListTitle_2}
-                tasks={tasks_2}
-            />
-
         </div>
     );
 }
