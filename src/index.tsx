f13970eac62b2fca87c8dda9ad00a4cd5f53a37c
ElenaBoyko1992 <Bionka@inbox.ru>
2022-11-20 20:28:27 +0300

    react has been updated

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,12 @@ import AppWithReducer from "./AppWithReducer";
 import {Provider} from "react-redux";
 import {store} from "./state/store";
 import AppWithRedux from "./AppWithRedux";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(<Provider store={store}><AppWithRedux/></Provider>, document.getElementById('root'));
+const container  = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+root.render(<Provider store={store}><AppWithRedux /></Provider>);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
