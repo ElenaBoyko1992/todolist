f9e0d8b1060dee88056ce70e4e08fdf4b2737bc1
ElenaBoyko1992 <Bionka@inbox.ru>
2023-05-19 10:04:00 +0300

    Updates

---
+++ precache-manifest.283e616eff4d38db93a406bfb83b40cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9925f2838bacac16990409a4bc14077",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "8f13fb6fd57a89d66d49",
+    "url": "/todolist/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f13fb6fd57a89d66d49",
+    "url": "/todolist/static/js/main.388542cc.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
